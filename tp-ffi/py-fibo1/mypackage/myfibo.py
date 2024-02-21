6d98e1194f60463270a3b4f9db3e36a8ab242115
Thomas OBEIN <obein005@etu.univ-littoral.fr>
2024-02-21 15:54:22 +0100

    tp-ffi: py-fibo1 - Organisation d'un projet Python

--- tp-ffi/py-fibo1/mypackage/myfibo.py
+++ tp-ffi/py-fibo1/mypackage/myfibo.py
@@ -3,5 +3,16 @@ def fibo_naive(n: int):
     assert n >= 0
     return n if n < 2 else fibo_naive(n-1) + fibo_naive(n-2)
 
-# TODO implement fibo_iterative
+def fibo_iterative(n: int):
+    if n == 0:
+        return 0
+    first = 0
+    second = 1
+    for i in range(n - 1):
+        tmp = second
+        second = first + second
+        first = tmp
+    return second
+    
+        
 
